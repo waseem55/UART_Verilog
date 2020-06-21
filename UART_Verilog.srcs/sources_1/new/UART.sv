9dbf339ab3c6cdc561d6326a67a39b59d7fb95c4
Waseem <waseem7446@gmail.com>
2020-06-20 23:51:46 -0500

    Initial commit

---
+++ UART_Verilog.srcs/sources_1/new/UART.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/20/2020 06:58:23 PM
+// Design Name: 
+// Module Name: UART
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UART(
+    input a
+    );
+endmodule
